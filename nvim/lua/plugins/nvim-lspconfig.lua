67f1edd96c41cab365a8846a685b2de0898f44c9
eclipseflower <eclipseflowernju@gmail.com>
2024-11-09 18:39:06 +0800

    lsp config update

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,18 +1,35 @@
 -- UnrealBuildTool.exe -mode=GenerateClangDatabase -Target=UnrealEditor Development Win64
 
 return {
-    -- "neovim/nvim-lspconfig",
-    -- opts = {
-    --     servers = {
-    --         lua_ls = {
-    --             settings = {
-    --                 Lua = {
-    --                     workspace = {
-    --                         maxPreload = 1,
-    --                     },
-    --                 },
-    --             },
-    --         },
-    --     },
-    -- },
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        workspace = {
+                            maxPreload = 100,
+                        },
+                    },
+                },
+            },
+
+            pyright = {
+                root_dir = function(fname)
+                    local util = require "lspconfig.util"
+                    local root_files = {
+                        "pyproject.toml",
+                        "setup.py",
+                        "setup.cfg",
+                        "requirements.txt",
+                        "Pipfile",
+                        "pyrightconfig.json",
+                        ".git",
+                        ".svn",
+                    }
+                    return util.root_pattern(unpack(root_files))(fname)
+                end,
+            },
+        },
+    },
 }

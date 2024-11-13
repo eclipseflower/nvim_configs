b1df99877c40610d59cea9e925efad9aa9d4b8ed
EclipseFlower <eclipseflowernju@gmail.com>
2024-11-13 22:21:53 +0800

    update neo-tree config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
-        { "<leader>fo", "<cmd>:Neotree<cr>", desc = "open neo tree" },
+        { "<leader>fo", "<cmd>:Neotree toggle<cr>", desc = "open neo tree" },
     },
     opts = {
         filesystem = {

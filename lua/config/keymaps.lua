e469f3ea2784bc4b065b8963e51ab9588aa068d2
eclipseflower <eclipseflowernju@gmail.com>
2024-12-09 15:34:00 +0800

    add svn log

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,6 +16,7 @@ map('n', '<leader>vC', string.format(svn_commit_str_format, '%:p'), { desc = 'SV
 map('n', '<leader>vc', function() return string.format(svn_commit_str_format, vim.fn.getcwd()) end, { expr = true, desc = 'SVN Commit (cwd)' })
 map('n', '<leader>vU', '<cmd>!svn update %:p<cr>', { desc = 'SVN Update (buffer)' })
 map('n', '<leader>vu', '<cmd>!svn update<cr>', { desc = 'SVN Update (cwd)' })
+map('n', '<leader>vl', '<cmd>!TortoiseProc /command:log /path:%:p<cr>', { desc = 'SVN Log (buffer)' })
 map('n', '<leader>vb',
     function()
         local revision = vim.fn.system('svn info --show-item revision')

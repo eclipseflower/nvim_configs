3bf8a2d57ecbae2d19f5029d121b613a1ca860d4
eclipseflower <eclipseflowernju@gmail.com>
2024-12-30 14:38:50 +0800

    add rm session

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 local map = vim.keymap.set
 
+-- normal mode
 map("n", "c", '"cc', { desc = "Change" })
 map("n", "C", '"cC', { desc = "Change" })
 map("n", "d", '"dd', { desc = "Delete" })
@@ -11,6 +12,10 @@ map("n", "D", '"dD', { desc = "Delete" })
 map("n", "x", '"xx', { desc = "Delete Char" })
 map("n", "X", '"xX', { desc = "Delete Char" })
 
+local rd_str_format = "<cmd>!rd /s/q %s<cr>"
+map("n", "<leader>qD", string.format(rd_str_format, vim.fn.stdpath("data") .. "\\shada"), { desc = "Delete All Sessions" })
+
+-- svn keymaps
 local svn_commit_str_format = "<cmd>!TortoiseProc /command:commit /path:%s<cr>"
 map("n", "<leader>vc", string.format(svn_commit_str_format, "%:p"), { desc = "SVN Commit (buffer)" })
 map("n", "<leader>vC", function()
@@ -26,6 +31,7 @@ map("n", "<leader>vb", function()
     return string.format("<cmd>!TortoiseProc /command:blame /path:%s /startrev:1 /endrev:%s<cr>", "%:p", revision)
 end, { expr = true, desc = "SVN Blame (buffer)" })
 
+-- lsp keymaps
 local function toggle_lsp()
     return Snacks.toggle({
         name = "Lsp (buffer)",
@@ -75,6 +81,8 @@ end
 
 toggle_lsp():map("<leader>ux")
 
+-- terminal mode
 map("t", "<c-v>", "<c-\\><c-n>pi", { desc = "Paste" })
 
+-- other mode
 map({ "i", "x", "s", "c" }, "<c-v>", "<c-r>*", { desc = "Paste" })
